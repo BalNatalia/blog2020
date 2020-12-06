042fe64bc7d36b51e2d7fd0e1c40752e210a9a0a
BalN757@mail.ru <BalN757@mail.ru>
2020-12-06 22:41:00 +0300

    задание 4

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,8 +6,17 @@ class Post < ApplicationRecord
 
   validates :title, presence: true, length: {in: 2..255}
   validates :body, presence: true
+  validate :check_user
 
   def edit_by?(current_user)
     current_user == user || current_user&.admin?
   end
+
+  private
+  def check_user
+  if new_record? && Post.where('user_id = ? and created_at > ?', user_id, 1.day.ago).count >= 5
+    errors.add(:base, 'Превышен лимит на максимальное количество сообщений за сутки')
+  end
+end
+
 end
